e03e00c29589c17d88a9c39cd62c0f0b3b1c4773
Owen Tuleja <owen@arborbridge.com>
2022-01-06 15:54:25 +0100

    wip

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 window.addEventListener("load", initiateApp());
 async function initiateApp() {
-  //YOU SHOULD CHANGE THIS TO YOUR API KEY
-  const owensApiKey = `fcb34b40-62a9-11ec-88d0-1becdba60906`;
+  const apiKey = `fcb34b40-62a9-11ec-88d0-1becdba60906`;
 
   let apiResponse = await fetch(
-    `https://app.zipcodebase.com/api/v1/search?apikey=${owensApiKey}&codes=01073&country=US`
+    `https://app.zipcodebase.com/api/v1/search?apikey=${apiKey}&codes=01073&country=US`
   );
   apiResponse = await apiResponse.json();
   console.log("response", apiResponse);
